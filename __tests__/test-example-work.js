3c71438e6327fbfa4c4537e9e4fde7119a0bd8fc
Patrick Lang <langpatrickr@johndeere.com>
2018-12-29 22:20:21 -0600

    final working portfolio

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -9,6 +9,8 @@ Enzyme.configure({ adapter: new Adapter() });
 const myWork = [
   {
     'title': "Work Example",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of a project involving code",
       'src': "images/example1.png",
@@ -17,6 +19,8 @@ const myWork = [
   },
   {
     'title': "Work Example",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of a project involving chemistry",
       'src': "images/example2.png",
@@ -26,19 +30,30 @@ const myWork = [
 ];
 
 describe("ExampleWork component", () => {
+
   let component = shallow(<ExampleWork work={myWork}/>);
-  it("Should be a 'section' element", () => {
-    expect(component.type()).toEqual('section');
+  it("Should be a 'span' element", () => {
+    expect(component.type()).toEqual('span');
   });
 
   it("Should contain as many children as there are work examples", () => {
     expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
   });
 
+  it("Should allow the modal to open and close", () => {
+    component.instance().openModal();
+    expect(component.instance().state.modalOpen).toBe(true);
+    component.instance().closeModal();
+    expect(component.instance().state.modalOpen).toBe(false);
+  });
+
 });
 
 describe("ExampleBubble component", () => {
-  let component = shallow(<ExampleWorkBubble example={myWork[1]}/>);
+  let mockOpenModalFn = jest.fn();
+
+  let component = shallow(<ExampleWorkBubble example={myWork[1]}
+    openModal={mockOpenModalFn}/>);
 
   let images = component.find("img");
 
@@ -50,4 +65,9 @@ describe("ExampleBubble component", () => {
     expect(images.prop('src')).toEqual(myWork[1].image.src);
   });
 
+  it("Should call the openModal handler when clicked", () => {
+    component.find(".section__exampleWrapper").simulate('click');
+    expect(mockOpenModalFn).toHaveBeenCalled;
+  });
+  
 });
